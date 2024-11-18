e4ed711fb4383c02c8864486f34bdf3e845edac1
yabokhar <yabokhar@student.42lyon.fr>
2024-11-18 01:53:42 +0100

    :D

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabokhar <yabokhar@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 01:51:32 by yabokhar          #+#    #+#             */
+/*   Updated: 2024/11/18 01:51:33 by yabokhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_swap(int *x, int *y)
+
+{
+	int	temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+				ft_swap(&tab[i], &tab[j]);
+			j++;
+		}
+		i++;
+	}
+}
